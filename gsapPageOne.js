2c042a3a459cda53239b4d77b4d1ba9bc83189a9
naren67 <naren678n@gmail.com>
2021-04-05 12:31:17 +0530

    first commit

--- gsapPageOne.js
+++ gsapPageOne.js
@@ -3,5 +3,7 @@ gsap.from('.nameMarquee',{opacity:0, duration:10, x:700, delay:8})
 gsap.from('.life',{opacity:0, duration:8, y:-1200, scale:5, delay:9})
 gsap.from('.github_btn',{opacity:0, duration:8, y:100, delay:13})
 gsap.from('.Name',{opacity:0, duration:6, y:-600, delay:12})
-gsap.from('.logo',{opacity:0, duration:8, y:100, delay:8})
+gsap.from('.hamburger',{opacity:0, duration:8, y:100, delay:13})
+gsap.from('.logo',{opacity:0, duration:8, y:100, delay:6})
+
 
