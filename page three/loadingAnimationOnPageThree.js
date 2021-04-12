2870bfd2757c1e1ab630fa6e3041a56d8fb0d6d0
naren67 <naren678n@gmail.com>
2021-04-12 10:55:25 +0530

    first commit

--- page three/loadingAnimationOnPageThree.js
+++ page three/loadingAnimationOnPageThree.js
@@ -1,3 +1,3 @@
 const time = gsap.timeline({defaults:{ease:'power1.out'}})
 
-time.to('.loadingScreen',{y:'-100%', duration:4, delay:2.5})
+time.to('.loadingScreen',{y:'-100%',opacity:0, duration:4, delay:6})
